f44aed931532f7336b8f4bd8049098796b65027e
george phillips <georgephillips@George-Mac.local>
2020-08-10 16:01:00 +0100

    Init

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/**
+ *  Todo List
+ *  1. List items
+ *      a. Completed
+ *      b. Title 
+ * 
+ *  Actions:
+ *  1. Create 
+ *  2. Mark Completed
+ *  3. Unmark completed
+ *  4. Edit Title
+ *  5. Delete
+ */
